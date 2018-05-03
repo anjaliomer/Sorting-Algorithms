ee635ee19f2b383d1c5d99bb319e63b5233cfeef
anjaliomer <anjaliom@buffalo.edu>
2018-05-02 23:10:36 -0400

    Added Bubble sort

---
+++ Bubble Sort/Bubble Sort/bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "bubble_sort.h"
+#include<iostream>
+
+using namespace std;
+
+void bubble_sort(int ar[], int size)
+{
+	print_array(ar, size);
+
+	for (int i = 0; i < size - 1;i++)
+	{
+		for (int j = 0; j < size - 1 - i;j++)
+		{
+			if (ar[j] > ar[j + 1])
+				swap(&ar[j], &ar[j + 1]);
+		}
+	}
+
+	print_array(ar, size);
+}
+
+void swap(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void print_array(int ar[], int size)
+{
+	for (int i = 0; i < size;i++)
+	{
+		cout << ar[i] << " ";
+	}
+	cout << "\n";
+}
+
---
+++ Bubble Sort/Bubble Sort/bubble_sort.h
@@ -0,0 +1,3 @@
+void bubble_sort(int[], int);
+void swap(int*, int*);
+void print_array(int[], int);
