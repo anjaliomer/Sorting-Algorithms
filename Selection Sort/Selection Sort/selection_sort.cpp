b120b81a15887e838bfea7ecbbbf067bedacf62c
anjaliomer <anjaliom@buffalo.edu>
2018-05-02 23:05:44 -0400

    Added Selection Sort

---
+++ Selection Sort/Selection Sort/selection_sort.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include "selection_sort.h"
+#include<iostream>
+
+using namespace std;
+
+void selection_sort(int ar[], int size)
+{
+	print_array(ar, size);
+	for (int i = 0; i < size - 1;i++)
+	{
+		int min_id = i;
+		for (int j = i + 1; j < size;j++)
+		{
+			if (ar[min_id] > ar[j])
+				min_id = j;
+		}
+		swap(&ar[min_id], &ar[i]);
+	}
+	print_array(ar, size);
+}
+
+void swap(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void print_array(int ar[], int size)
+{
+	for (int i = 0; i < size;i++)
+	{
+		cout << ar[i] << " ";
+	}
+	cout << "\n";
+}
---
+++ Selection Sort/Selection Sort/selection_sort.h
@@ -0,0 +1,3 @@
+void selection_sort(int[], int);
+void swap(int*, int*);
+void print_array(int[], int);
