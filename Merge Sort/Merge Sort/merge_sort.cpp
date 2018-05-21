be9754afce9004495fb21a1ebef683807968066b
anjaliomer <anjaliom@buffalo.edu>
2018-05-21 18:26:54 -0400

    Added Merge Sort

--- Merge Sort/Merge Sort/merge_sort.cpp
+++ Merge Sort/Merge Sort/merge_sort.cpp
@@ -4,8 +4,71 @@
 
 using namespace std;
 
-void merge_sort(int ar[], int size)
+void merge_sort(int ar[], int first, int last)
 {
+	if (first < last)
+	{
+		int mid = first + (last - first) / 2;
+		merge_sort(ar, first, mid);
+		merge_sort(ar, mid + 1, last);
+		merge(ar, first, last, mid);
+	}
+}
+
+void merge(int ar[], int first, int last, int mid)
+{
+	int nl = mid - first + 1;
+	int nr = last - mid;
+
+	int * l = new int[nl];
+	int * r = new int[nr];
+
+	for (int i = 0; i < nl; i++)
+	{
+		l[i] = ar[first + i];
+	}
+
+	for (int j = 0; j < nr; j++)
+	{
+		r[j] = ar[mid + 1 + j];
+	}
+
+	int i = 0;
+	int j = 0;
+	int k = first;
+
+	while (i < nl && j < nr)
+	{
+		if (l[i] <= r[j])
+		{
+			ar[k] = l[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			ar[k] = r[j];
+			j++;
+			k++;
+		}
+	}
+
+	while (i < nl)
+	{
+		ar[k] = l[i];
+		i++;
+		k++;
+	}
+
+	while (j < nr)
+	{
+		ar[k] = r[j];
+		j++;
+		k++;
+	}
+
+
+
 
 }
 
--- Merge Sort/Merge Sort/merge_sort.h
+++ Merge Sort/Merge Sort/merge_sort.h
@@ -1,2 +1,3 @@
-void merge_sort(int[], int);
+void merge_sort(int[], int, int);
+void merge(int[], int, int, int);
 void print_array(int[], int);
