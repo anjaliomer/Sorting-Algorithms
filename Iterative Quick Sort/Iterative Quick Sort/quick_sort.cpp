d5d165ef81dd483763805a82ffdbe2d705f433a7
anjaliomer <anjaliom@buffalo.edu>
2018-05-28 15:52:35 -0400

    Added Iterative Quick Sort

--- Iterative Quick Sort/Iterative Quick Sort/quick_sort.cpp
+++ Iterative Quick Sort/Iterative Quick Sort/quick_sort.cpp
@@ -1,36 +1,35 @@
 #include "stdafx.h"
 #include "quick_sort.h"
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 void quick_sort(int ar[], int first, int last)
 {
-	if (first < last)
-	{
-		int pr = partition(ar, first, last);
-		quick_sort(ar, pr + 1, last);
-		quick_sort(ar, first, pr - 1);
+	int *stack = new[last-first+1];
+
+	int pi = partition(ar, first, last);
+		if(pi)
+
 	}
+	
 }
 
 int partition(int ar[], int first, int last)
 {
 	int pivot = ar[last];
-	int i = first-1;
+	int i = first - 1;
 	for (int j = first; j < last; j++)
 	{
 		if (ar[j] <= pivot)
-		{ 
 			i++;
-			swap(&ar[i], &ar[j]);
-		}	
+		swap(&ar[i], &ar[j]);
 	}
 	swap(&ar[i + 1], &ar[last]);
 	return (i + 1);
 }
 
-void swap(int *a, int*b)
+void swap(int*a, int*b)
 {
 	int t = *a;
 	*a = *b;
